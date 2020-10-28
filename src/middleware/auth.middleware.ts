e4ef53989c156eedd81dd0428b008f43a233c1f3
Ankit Pant <ankit.pant@klm.com>
2020-10-28 22:51:36 +0100

    remove dead code

--- src/middleware/auth.middleware.ts
+++ src/middleware/auth.middleware.ts
@@ -4,9 +4,9 @@ export async function useFakeAuthAuthMiddleware(req: any, res: any, next: any) {
     if (!req.session?.user) {
       // Use fake user
       req.session.user = {
-        username: "dummyName",
-        displayName: "Dummy User",
-        email: "dummy.user@domain.com",
+        username: 'dummyName',
+        displayName: 'Dummy User',
+        email: 'dummy.user@domain.com',
       };
     }
   } catch (error) {
@@ -18,18 +18,7 @@ export async function useFakeAuthAuthMiddleware(req: any, res: any, next: any) {
 
 export async function useAuthAuthMiddleware(req: any, res: any, next: any) {
   try {
-    // If this user already has a session, return
-    if (!req.session?.user) {
-      res.status(401).json({ message: "invalid users" });
-      // setTimeout(() => {
-      //   console.log("Faking authentication request");
-      //   req.session.user = {
-      //     username: "ankitpant",
-      //     displayName: "Ankit Pant",
-      //     email: "ankit12.pant@gmail.com",
-      //   };
-      // }, 10000);
-    }
+    // @todo implement auth flow
   } catch (error) {
     console.error(error);
   } finally {
